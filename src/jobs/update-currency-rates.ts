1d6740ea81189282b1998b0be9d471bf5e73ae02
anubus298 <araristaf@gmail.com>
2025-09-16 22:04:41 +0100

    feat: added CURRENCY_UPDATE_SCHEDULE env to customize cron schedule

--- src/jobs/update-currency-rates.ts
+++ src/jobs/update-currency-rates.ts
@@ -1,14 +1,16 @@
 // src/jobs/update-currency-rates.ts
-import { MedusaContainer } from "@medusajs/framework/types"
-import { updatePricesWithExchangeRates } from "../workflows/update-prices-with-exchange-rates"
+import { MedusaContainer } from "@medusajs/framework/types";
+import { updatePricesWithExchangeRates } from "../workflows/update-prices-with-exchange-rates";
 
-export default async function updateCurrencyRatesJob(container: MedusaContainer) {
-  const logger = container.resolve("logger")
-  logger.info("Run update-currency-rates-job")
-  await updatePricesWithExchangeRates(container).run()
+export default async function updateCurrencyRatesJob(
+  container: MedusaContainer
+) {
+  const logger = container.resolve("logger");
+  logger.info("Run update-currency-rates-job");
+  await updatePricesWithExchangeRates(container).run();
 }
 
 export const config = {
   name: "update-currency-rates-job",
-  schedule: "0 0 * * *",
-}
+  schedule: process.env.CURRENCY_UPDATE_SCHEDULE || "0 0 * * *",
+};
